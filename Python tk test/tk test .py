799103876047d27931b53db46ed7677cd3af785a
sara369 <47646689+sara369@users.noreply.github.com>
2022-11-04 20:21:50 -0700

    Add files via upload

---
+++ Python tk test/tk test .py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+tk = Tk()
+frame = Frame(tk, borderwidth=2)
+frame.pack(fill=BOTH, expand=1)
+label = Label(frame, text="Button Example")
+label.pack(fill=X, expand=1)
+
+button = Button(frame, text="Exit", command=tk.destroy)
+button.pack(side=BOTTOM)
+tk.mainloop()
